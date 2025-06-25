7f9093da7ac2773be9ca523c99519ae02b34ba8e
Jack Waller <jack.waller94@gmail.com>
2025-06-25 13:47:07 +1000

    chore: add packages

--- pipelines/pipelines/schedules/__init__.py
+++ pipelines/pipelines/schedules/__init__.py
@@ -3,10 +3,10 @@ from ..jobs import daily_trades_job
 
 @schedule(
     job=daily_trades_job, 
-    cron_schedule="* * * * *",
+    cron_schedule="*/5 * * * *",
     execution_timezone="UTC"
 )
 def daily_schedule(context: ScheduleEvaluationContext):
-    """Update our daily trades data every minute"""
+    """Update our daily trades data every 5 minutes"""
     day_to_process = context.scheduled_execution_time.strftime("%Y-%m-%d")
     return RunRequest(run_key=None, partition_key=day_to_process)
