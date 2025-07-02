7566e1d39860e6c0972e62282b74857b73731e49
Jack Waller <jack.waller94@gmail.com>
2025-07-02 13:14:18 +1000

    chore: change jobs to use upstream dependencies

--- pipelines/pipelines/jobs/__init__.py
+++ pipelines/pipelines/jobs/__init__.py
@@ -4,14 +4,13 @@ from ..partitions import daily_partitions, dlob_start_daily_partitions
 daily_trades_job = define_asset_job(
     name="daily_trades_job",
     partitions_def=daily_partitions,
-    selection=AssetSelection.keys("raw_trades") | AssetSelection.groups("trades"),
+    selection=AssetSelection.keys("top_makers", "daily_market_summary").upstream(),
 )
 
 daily_trigger_order_report_job = define_asset_job(
     name="daily_trigger_order_report",
     partitions_def=dlob_start_daily_partitions,
     selection=(
-        AssetSelection.keys("raw_trigger_orders", "raw_actions").downstream()
-        | AssetSelection.keys("raw_trades", "clean_trades")
+        AssetSelection.keys("triggered_order_summary").upstream()
     ),
 )
